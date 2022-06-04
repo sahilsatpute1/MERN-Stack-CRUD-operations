913aaef4a6be5a3420b27cffdc73cd3e3f495555
Sahil Satpute <sahilsatpute05@gmail.com>
2022-06-04 22:12:53 +0530

    Mern Stack CRUD operations

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+
+const FoodModel = require('./models/Food')
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://sahil:sahilmongo@cluster0.qf1qlkt.mongodb.net/food?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+});
+
+app.post('/insert', async (req, res) => {
+    const foodName = req.body.foodName;
+    const days = req.body.days;
+
+    const food = new FoodModel({foodName: foodName, daysSinceIAte: days})
+
+    try {
+        await food.save();
+        res.send("inserted data")
+    } 
+    catch(err){
+        console.log(err);
+    }
+})
+
+app.get('/read', async (req, res) => {
+    FoodModel.find({}, (err, result) => {
+        if(err) {
+            res.send(err)
+        }
+        res.send(result)
+    })
+})
+
+app.put('/update', async (req, res) => {
+    const newFoodName = req.body.newFoodName;
+    const id = req.body.id;
+
+    try {
+        await FoodModel.findById(id, (err, updatedFood) => {
+            updatedFood.foodName = newFoodName;
+            updatedFood.save();
+            res.send("Updated Successfully")
+        })
+    } 
+    catch(err){
+        console.log(err);
+    }
+})
+
+app.delete('/delete/:id', async (req, res) => {
+    const id = req.params.id;
+    await FoodModel.findByIdAndRemove(id).exec();
+    res.send("Deleted successfully")
+})
+
+app.listen(3001, ()=> {
+    console.log('Server running at port 3001');
+})
